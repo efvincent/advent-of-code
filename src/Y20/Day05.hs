55c6a1bed1bc1d84fa66d055c7b4f5909a3a888d
Eric Vincent <eric.vincent@bluecore.com>
2020-12-05 03:30:41 -0500

    cleaned out redundant / unneeded code

--- src/Y20/Day05.hs
+++ src/Y20/Day05.hs
@@ -1,33 +1,19 @@
 module Y20.Day05 where
-import qualified Data.Set as Set
+import Data.List (sort)
 
 filename :: [Char]
 filename = "./data/20/Day05.txt"
-
-{-
-not the original approach, switched after realizing the string was just
-binary. This is so much nicer.
--}
 
 decode :: [Char] -> [Int]
 decode = map (\c -> if c == 'F' || c == 'L' then 0 else 1)
 
 binToInt :: [Int] -> Int
 binToInt = foldl (\acc n -> 2 * acc + n) 0
- 
+
 solve5 :: IO ()
 solve5 = do
   raw <- readFile filename
   print $ maximum $ map (binToInt . decode) $ lines raw
-
-{-
-part 2
-front and rear rows don't count... so:
-1. generate a set of codes from raw data
-2. remove the codes from first and last rows
-3. scan the remaining list in order until you find consecutive seats with a gap. 
-  the gap is the answer
--}
 
 findGap :: [Int] -> Int
 findGap [] = 0
@@ -37,8 +23,4 @@ findGap (_:y:xs) = findGap (y:xs)
 solve5b :: IO ()
 solve5b = do
   raw <- readFile filename
-  let ordered = 
-        Set.toAscList
-        $ foldr Set.delete (Set.fromList $ map (binToInt . decode) $ lines raw)
-        $ [0..7] ++ [127 * 8 + c | c <- [0..7]] in
-    print $ findGap ordered
+  print $ findGap $ sort $ map (binToInt . decode) $ lines raw 
