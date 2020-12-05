90ad8024441b6f2ca777fbbc498dfa37c3bac00b
Eric Vincent <eric.vincent@bluecore.com>
2020-12-05 01:35:44 -0500

    cleaned up, comments

--- src/Y20/Day05.hs
+++ src/Y20/Day05.hs
@@ -1,28 +1,24 @@
 module Y20.Day05 where
 import qualified Data.Set as Set
 
-data Axis = ROW | COL
-
 filename :: [Char]
 filename = "./data/20/Day05.txt"
 
-stepRow :: [Char] -> Int -> Int -> Int
-stepRow ['F'] low _ = low
-stepRow ['B'] _ hi = hi
-stepRow ('F':cs) low hi = stepRow cs low (((hi-low) `div` 2) + low)
-stepRow ('B':cs) low hi = stepRow cs (((hi-low) `div` 2) + 1 + low) hi
+-- | c1 and c2 are the encoding chars, either 'F' 'B' or 'L' 'R'. This function 
+-- pattern matches the `[Char]` list. When there's only one char left `[c]` the
+-- guard chooses either the low or high remaining value. Otherwise, it either 
+-- picks the top or bottom half and recurses
+step :: Char -> Char -> [Char] -> Int -> Int -> Int
+step c1 _  [c] low _ | c == c1 = low
+step _  c2 [c] _ hi  | c == c2 = hi
+step c1 c2 (c:cs) low hi | c == c1 = step c1 c2 cs low (((hi-low) `div` 2) + low)
+step c1 c2 (c:cs) low hi | c == c2 = step c1 c2 cs (((hi-low) `div` 2) + 1 + low) hi
 
-stepCol :: [Char] -> Int -> Int -> Int
-stepCol ['L'] low _ = low
-stepCol ['R'] _ hi = hi
-stepCol ('L':cs) low hi = stepCol cs low (((hi-low) `div` 2) + low) 
-stepCol ('R':cs) low hi = stepCol cs (((hi-low) `div` 2) + 1 + low) hi 
-  
 code :: Int -> Int -> Int
 code r c = r * 8 + c
 
 findSeat :: [Char] -> Int
-findSeat cs = code (stepRow (take 7 cs) 0 127) (stepCol (drop 7 cs) 0 7)
+findSeat cs = code (step 'F' 'B' (take 7 cs) 0 127) (step 'L' 'R' (drop 7 cs) 0 7)
 
 solve5 :: String -> IO ()
 solve5 fn = do
@@ -35,7 +31,7 @@ front and rear rows don't count... so:
 1. generate a set of codes from raw data
 2. remove the codes from first and last rows
 3. scan the remaining list in order until you find consecutive seats with a gap. 
-   the gap is the answer
+  the gap is the answer
 -}
 
 findGap :: [Int] -> Int
@@ -46,8 +42,8 @@ findGap (_:y:xs) = findGap (y:xs)
 solve5b :: IO ()
 solve5b = do
   raw <- readFile filename
-  let codes = Set.fromList $ map findSeat $ lines raw 
-  let codes' = foldr Set.delete codes $ [code 0 c | c <- [0..7]] ++ [code 127 c | c <- [0..7]] 
-  let ordered = Set.toAscList codes' in
+  let ordered = 
+        Set.toAscList
+        $ foldr Set.delete (Set.fromList $ map findSeat $ lines raw)
+        $ [code 0 c | c <- [0..7]] ++ [code 127 c | c <- [0..7]] in
     print $ findGap ordered
-    
