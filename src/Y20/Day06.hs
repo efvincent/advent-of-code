5dc61f5dcad53db2a02a2c7a1eca15201c93fb10
Eric Vincent <eric.vincent@bluecore.com>
2020-12-06 20:34:43 -0500

    not going with flow

--- src/Y20/Day06.hs
+++ src/Y20/Day06.hs
@@ -3,6 +3,8 @@ module Y20.Day06 where
 import qualified Data.Set as Set
 import Data.List (intersect)
 import Data.List.Split (splitOn)
+import Data.Function ( (&) ) 
+import Flow ((|>))
 
 filename6 :: String
 filename6 = "./data/20/Day06.txt"
@@ -13,10 +15,22 @@ solve61 = do
   let groups = map (concat . splitOn "\n") $ splitOn "\n\n" raw 
   print  $ sum $ map (length . Set.fromList) groups 
 
+solve61flow :: IO ()
+solve61flow = do
+  raw <- readFile filename6
+  splitOn "\n\n" raw 
+    & map (concat . splitOn "\n") 
+    & map (length . Set.fromList) 
+    & sum 
+    & print
+
 solve62 :: IO ()
 solve62 = do
   raw <- readFile filename6
-  let groups = map
-                (splitOn "\n")
-                (splitOn "\n\n" raw)
-  print $ sum $ map length $ filter (/= "") $ map (foldl1 intersect) groups
+  let groups = 
+        map (splitOn "\n") (splitOn "\n\n" raw) 
+        |> map (foldl1 intersect) 
+        |> filter (/= "") 
+        |> map length 
+        |> sum
+  print groups
