0276335062ad0005f030f02da1457358509ef6f1
Eric Vincent <eric.vincent@bluecore.com>
2020-12-14 20:37:36 -0500

    day 14 solution 2

--- src/Y20/Day14.hs
+++ src/Y20/Day14.hs
@@ -1,21 +1,27 @@
-module Y20.Day14 where
+{-# LANGUAGE LambdaCase #-}
+module Y20.Day14 (solve20d14p2, solve20d14p1) where
 
 import qualified Data.IntMap as M
--- import Data.List (isPrefixOf)
-import Text.Regex.TDFA
+import Text.Regex.TDFA ( (=~), AllTextMatches(getAllTextMatches) )
+import Util (genBitPermutations)
+
+type Bin = M.IntMap Int
+type Memory = M.IntMap Int
+type Mask = [(M.Key, Int)]
 
 data State = State
-  { _mask :: [(M.Key, Int)] 
-  , _mem :: M.IntMap Int }
+  { _masks :: [Mask] 
+  , _mem :: Memory }
   deriving (Show)
 
-data Op = Mask [(M.Key, Int)]
-        | Write Int (M.IntMap Int)
+data Op = Mask [Mask]
+        | Write Int Bin
         deriving (Show)
 
-initState = State { _mask = [], _mem = M.empty }
+initState :: State
+initState = State { _masks = [], _mem = M.empty }
 
-toBin :: Int -> M.IntMap Int
+toBin ::  Int -> Bin
 toBin n =
   let b = toBinary n
   in M.fromList $ zip [35,34..0] $ replicate (36 - length b) 0 ++ b
@@ -24,19 +30,39 @@ toBin n =
     toBinary 0 = [ 0 ]
     toBinary n = toBinary ( n `quot` 2 ) ++ [ n `rem` 2 ]
 
-fromBin :: M.IntMap Int -> Int
+fromBin :: Bin -> Int
 fromBin m =
   foldl (\acc (p,bit) -> acc + (bit * (2^p))) 0 l
   where
     l = M.toList m
 
-toMask :: String -> [(Int,Int)]
-toMask s =
-  filter (\(_,b) -> b /= (-1)) 
+-- | First interpretation of the mask. X is unused and ignored. 0 or 1 overwrite
+-- the target value and are recorded as is
+toMask1 :: String -> [Mask]
+toMask1 s =
+  [filter ((/= -1) . snd)  
     $ zip [35,34..0] 
-    $ map (\c -> if c == 'X' then -1 else read [c]) s
+    $ map (\c -> if c == 'X' then -1 else read [c]) s]
 
-applyMask :: M.IntMap a -> [(M.Key, a)] -> M.IntMap a
+-- | Second interpretation of the mask. 0 is unused and ignored. X is meant to 
+-- expand into both 1 and 0, therefore toMask2 expands to a list of masks to be
+-- applied
+toMask2 :: String -> [Mask]
+toMask2 s =
+  map M.toList masks
+  where
+  rawMask = 
+    filter ((/= 0) . snd) 
+    $ zip [35,34..0] 
+    $ map (\case 
+        'X' -> 2
+        c -> read [c]) s
+  template = M.fromList rawMask
+  indexes = map fst $ filter (\(_,n) -> n == 2) rawMask
+  mods = map (zip indexes) $ genBitPermutations $ length indexes
+  masks = foldl (\acc' mod ->  let mask = foldl (\acc (k,v) -> M.insert k v acc) template mod in mask:acc') [] mods
+
+applyMask :: Bin -> Mask -> Bin
 applyMask = foldl (\vals (k,v) -> M.insert k v vals)
 
 parseWrite :: String -> Op
@@ -48,13 +74,42 @@ parseWrite s =
     b = read v :: Int
     bin = toBin b
 
-parseLine :: String -> Op 
-parseLine s 
-  | s =~ "mask" = Mask $ toMask $ s =~ "[X01]{36}"
+parseLine :: (String -> [Mask]) -> String -> Op 
+parseLine toMaskFn s 
+  | s =~ "mask" = Mask $ toMaskFn $ s =~ "[X01]{36}"
   | s =~ "mem"  = parseWrite s
   
-runOp :: State -> Op -> State
-runOp s (Mask m) = s { _mask = m }
-runOp s@State{_mem=mem, _mask=mask} (Write addr num) = 
-  s { _mem = M.insert addr (fromBin $ applyMask num mask) mem }
+runOp1 :: State -> Op -> State
+runOp1 s (Mask m) = s { _masks = m }
+runOp1 s@State{_mem=mem, _masks=mask} (Write addr num) = 
+  s { _mem = M.insert addr (fromBin $ applyMask num $ head mask) mem }
 
+runOp2 :: State -> Op -> State
+runOp2 s (Mask m) = s { _masks = m }
+runOp2 s@State{_mem=mem, _masks=masks } (Write addr num) =
+  s { _mem = foldl doOne mem masks }
+  where
+    addrBin = toBin addr
+    num' = fromBin num
+    doOne :: Memory -> Mask -> Memory
+    doOne mem' mask =
+      let addr' = fromBin $ applyMask addrBin mask
+      in M.insert addr' num' mem'
+
+solve :: (String -> [Mask]) -> (State -> Op -> State) -> String -> Int
+solve toMask runOp raw =
+  let ops = map (parseLine toMask) $ lines raw
+  in let State{_mem=mem} = foldl runOp initState ops
+  in sum $ map snd $ M.toList mem
+
+solve20d14p1 :: IO ()
+solve20d14p1 = do
+  raw <- readFile "./Data/20/Day14.txt"
+  let ans = solve toMask1 runOp1 raw
+  print ans
+  
+solve20d14p2 :: IO () -- (3.10 secs, 6,303,551,160 bytes)
+solve20d14p2 = do
+  raw <- readFile "./Data/20/Day14.txt"
+  let ans = solve toMask2 runOp2 raw
+  print ans
