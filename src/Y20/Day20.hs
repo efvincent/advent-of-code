e697beb3abef883cf7b582cfe7de3e91224cc227
Eric Vincent <eric@efvincent.com>
2020-12-22 01:39:23 -0500

    solve d 20 p1

--- src/Y20/Day20.hs
+++ src/Y20/Day20.hs
@@ -2,12 +2,18 @@ module Y20.Day20 where
 
 import Data.List.Split (splitOn)
 import qualified Data.IntMap as IM
-import Data.List.Extra (intercalate)
+import Data.List.Extra (intersect, intercalate)
 import Linear.V2 ( V2(..) )
 import Linear.V4 ( V4(..) )
 
 type Point = V2 IM.Key
-type Block = IM.IntMap (IM.IntMap Char)
+type Row = IM.IntMap Char
+type Grid = IM.IntMap Row
+type Blocks = IM.IntMap Block
+data Block = Block
+  { _num :: Int
+  , _grid :: Grid }
+  deriving (Show)
 
 fn1 :: [Char]
 fn1 = "./Data/20/Day20.ex01.txt"
@@ -15,33 +21,55 @@ fn :: [Char]
 fn = "./Data/20/Day20.txt"
 
 blockToStr :: Block -> String
-blockToStr b =  intercalate "\n" $ map (map snd . IM.toList . (b IM.!)) [0..9] 
+blockToStr b =  intercalate "\n" $ map (map snd . IM.toList . (_grid b IM.!)) [0..9] 
     
 bitListToInt :: [(Int, Char)] -> Int
 bitListToInt = sum . map (\(n,c) -> if c == '#' then 2^n else 0)
 
-blockSigs :: Block -> V4 Int
+blockSigs :: Block -> [Int]
 blockSigs b =
-  V4 s0 s1 s2 s3
+  [s0, s1, s2, s3, s0', s1', s2', s3']
   where
-    s0 = bitListToInt . IM.toList . (IM.! 0) $ b
+    g = _grid b
+    s0 = bitListToInt . IM.toList . (IM.! 0) $ g
     s1 = bitListToInt $ zip [0..] $ [getP b (V2 9 x) | x <- [0..9]]
-    s2 = bitListToInt . IM.toList . (IM.! 9) $ b
+    s2 = bitListToInt . IM.toList . (IM.! 9) $ g
     s3 = bitListToInt $ zip [0..] $ [getP b (V2 0 x) | x <- [0..9]]
 
-parseBlock :: String -> (Int, Block)
+    s0' = bitListToInt . zip [9,8..]. map snd . IM.toList . (IM.! 0) $ g
+    s1' = bitListToInt . zip [9,8..] $ [getP b (V2 9 x) | x <- [0..9]]
+    s2' = bitListToInt . zip [9,8..]. map snd . IM.toList . (IM.! 9) $ g
+    s3' = bitListToInt . zip [9,8..] $ [getP b (V2 0 x) | x <- [0..9]]    
+
+blockMatch b1 b2 =
+  (not . null) (blockSigs b1 `intersect` blockSigs b2)
+  where
+    bs1 = blockSigs b1
+    bs2 = blockSigs b2
+
+matchCount :: Blocks -> Int -> Int
+matchCount bm bnum = 
+  length . filter id . map (blockMatch b . snd) . IM.toList . IM.delete bnum $ bm 
+  where 
+    b = bm IM.! bnum
+
+parseBlock :: String -> Block
 parseBlock raw =
-  (num, IM.fromList . zip [0..] . map mkRow $ ss)
+  Block { _num = num, _grid = IM.fromList . zip [0..] . map mkRow $ ss}
   where
     (s:ss) = lines raw
     num = read . take 4 . drop 5 $ s
     mkRow s =  IM.fromList $ zip [0..] s
 
-parseRaw :: String -> IM.IntMap Block
+parseRaw :: String -> Blocks
 parseRaw raw =
-  IM.fromList . map parseBlock $ blocks
-  where
-    blocks = splitOn "\n\n" raw
+  IM.fromList . map (\s -> let b = parseBlock s in (_num b, b)) 
+  $ splitOn "\n\n" raw
 
 getP :: Block -> Point -> Char
-getP b (V2 x y) = (IM.! y) b IM.! x 
+getP b (V2 x y) = (IM.! y) (_grid b) IM.! x 
+
+solve20d20p1 fn = do
+  raw <- readFile fn
+  let bm = parseRaw raw in
+    print $ product $ map fst $ filter (\(_,c) -> c == 2) $ map (\n -> (n,matchCount bm n)) $ IM.keys bm
