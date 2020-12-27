881026e19ad22f42cb84447d66c978c6719992c6
Eric Vincent <eric.vincent@bluecore.com>
2020-12-27 01:01:26 -0500

    day 22 & part 1 of 23

--- src/Y20/Day22.hs
+++ src/Y20/Day22.hs
@@ -1,26 +1,71 @@
-module Y20.Day22 (solve20d22p1) where
+module Y20.Day22 (solve20d22p2,solve20d22p1)  where
 
 import Data.List.Split (splitOn)
 import Debug.Trace (trace)
+import qualified Data.IntSet as IS
+import Data.IntSet (IntSet)
+import qualified Data.Map as M
+import Data.Map (Map)
+import Data.Sequence (Seq(..), (|>), fromList)
+import qualified Data.Sequence as SEQ
+import Data.Foldable (toList)
+import Data.Hashable (hash)
+
+type Deck = Seq Int
 
 fn :: [Char]
 fn = "./Data/20/Day22.txt"
 
-parseRaw :: [Char] -> [(Integer, [String])]
-parseRaw = zip [1..] . map (tail . lines) . splitOn "\n\n"
+parseRaw :: [Char] -> [(Integer, Deck)]
+parseRaw = 
+  zip [1..] . map (fromList . map read . tail . lines) . splitOn "\n\n" 
+  
+score :: Deck -> Int
+score xs = sum . zipWith (*) [length xs, length xs-1 .. 1] $ toList xs
 
-play :: Num a => [(b, [String])] -> (a, String, Int)
-play g =
-  let [p1,p2] = map (map read . snd) g in go 1 p1 p2
+play1 ::[(b, Deck)] -> (Int, String, Int)
+play1 g =
+  let [p1,p2] = map snd g in go 1 p1 p2
   where
-    score xs = sum . zipWith (*) [length xs, length xs-1 .. 1] $ xs
-    go c (x:xs) (y:ys)
-      | x > y = go (c+1) (xs ++ [x,y]) ys
-      | y > x = go (c+1) xs (ys ++ [y,x])
-    go c p1@(x:xs) [] = (c, "player 1", score p1)
-    go c [] p2@(y:ys) = (c, "player 2", score p2)
+    go :: Int -> Deck -> Deck -> (Int, String, Int)
+    go c (x :<| xs) (y :<| ys)
+      | x > y = go (c+1) (xs |> x |> y) ys
+      | y > x = go (c+1) xs (ys |> y |> x)
+    go c p1 _ | not . null $ p1 = (c, "player 1", score p1)
+    go c _ p2 | not . null $ p2 = (c, "player 2", score p2)
+
+mkHash :: Deck -> Deck -> Int
+mkHash xs ys = hash (take 2 (toList xs), take 2 (toList ys), length xs)
+
+play2 :: [(a, Deck)] -> (Int, Int)
+play2 g =
+  let [p1,p2] = map snd g in 
+  let (winner, hand) = go IS.empty p1 p2 in
+  (winner, score hand) 
+  where
+    go :: IntSet -> Deck -> Deck -> (Int, Deck)
+    go prevHands p1@(x :<| xs) p2@(y :<| ys)
+      | IS.member (mkHash p1 p2) prevHands = (1, p1)
+      | length xs >= x && length ys >= y = 
+        let prevHands' = IS.insert (mkHash p1 p2) prevHands in
+        let (winner, wh) = go prevHands' (SEQ.take x xs) (SEQ.take y ys) in
+        if winner == 1 
+            then go prevHands' (xs :|> x :|> y) ys
+            else go prevHands' xs (ys :|> y :|> x) 
+      | otherwise =
+        let prevHands' = IS.insert (mkHash p1 p2) prevHands in
+        if x > y 
+          then go prevHands' (xs :|> x :|> y) ys
+          else go prevHands' xs (ys :|> y :|> x)
+    go _ p1 _ | not . null $ p1 = (1, p1)
+    go _ _ p2 | not . null $ p2 = (2, p2)
 
 solve20d22p1 :: IO ()
 solve20d22p1 = do
   raw <- readFile fn
-  print $ play $ parseRaw raw
+  print $ play1 $ parseRaw raw
+
+solve20d22p2 :: IO ()
+solve20d22p2 = do
+  raw <- readFile fn
+  print $ play2 $ parseRaw raw
--- src/Y20/Day23.hs
+++ src/Y20/Day23.hs
@@ -1,8 +1,86 @@
 module Y20.Day23 where
 
 import qualified  Data.CircularList as CL
+import Data.CircularList (rotL, insertL, rotNR, rotNL, rotR, removeR, insertR, focus, rotateTo)
+import Debug.Trace
+import Data.Traversable (for)
+import Data.Foldable (Foldable(foldl'))
 
+type Cups = CL.CList Int
+
+t :: [Char]
 t = "583976241"
+t1 :: [Char]
+t1 = "389125467"
 
-parseRaw :: [Char] -> CL.CList Int
+parseRaw :: [Char] -> Cups
 parseRaw = CL.fromList . map (read . (:[])) 
+
+parseRaw2 :: [Char] -> Cups
+parseRaw2 s = 
+  let xs = map (read . (:[])) s in
+  let maxx = maximum xs in
+  CL.fromList $ xs ++ [(maxx + 1) .. 1000000]
+
+popR :: CL.CList a -> (Maybe a, CL.CList a)
+popR cl = (focus cl, removeR cl)
+
+pickUp :: Int -> Cups -> ([Int], Cups)
+pickUp c cups = 
+  let Just (hand, cups') = go [] c (rotR cups) in
+    (reverse hand, cups')
+  where
+    go acc 0 cl = Just (acc, cl)
+    go acc c cl = do
+      n <- focus cl 
+      let cl' = removeR cl
+      go (n:acc) (c-1) cl'
+
+curOf :: CL.CList a -> a
+curOf cups = let Just cur = focus cups in cur
+
+findDest 
+  :: Int        -- ^ minumum value. If we're below here, wrap to max 
+  -> Int        -- ^ max value. Wrap here if target is below min
+  -> Int        -- ^ target we're looking for
+  -> [Int]      -- ^ cups in hand - target cannot be these
+  -> Cups       -- ^ the current circle of cups; we'll be rotating them in recursive calls
+  -> Maybe Cups -- ^ finally the configuration when we're on target
+findDest mn mx target hand cups
+  | target < mn        = findDest mn mx mx         hand cups
+  | target `elem` hand = findDest mn mx (target-1) hand cups 
+  | otherwise =
+    case rotateTo target cups of
+      Nothing -> findDest mn mx (target -1) hand cups
+      ans -> ans
+  
+push :: Cups -> [Int] -> Cups
+push cups xs = 
+  let cups' = foldl (\acc x -> rotL $ insertL x acc) cups xs in
+  cups'
+
+turn :: Int -> Int -> Cups -> Cups
+turn minN maxN cups = 
+  cups'
+  where
+    cur = curOf cups
+    (hand, cups') = 
+      let (h,cs) = pickUp 3 cups in
+      let target = cur - 1 in
+      let Just dest = findDest minN maxN target h cs in
+      let inserted = insertL (hand !! 2) . insertL (hand !! 1) . insertL (head hand) $ dest in
+      let Just backToCur = rotateTo cur inserted in
+      (h,rotR backToCur)
+
+takeNturns :: (Enum a, Num a) => a -> Cups -> Cups
+takeNturns n cups = 
+  foldl' (\acc _ -> turn minN maxN acc) cups [1..n]
+  where
+    minN = minimum cups
+    maxN = maximum cups
+    
+
+solve20d23p1 :: Int -> String -> IO ()
+solve20d23p1 n raw = 
+  let Just cups' = rotateTo 1 . takeNturns n . parseRaw $ raw in
+  print . concatMap show . tail . CL.toList $ cups'
